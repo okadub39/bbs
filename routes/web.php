2a8830f9d2671c8534def3ffbc3636f0fe4982f4
岡田敏裕 <web@nuevo.jp>
2020-07-14 21:33:03 +0900

    投稿ページ作成

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostController');
+Route::resource('comments', 'CommentController');
